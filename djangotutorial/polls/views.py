9391948f9f3cb7c11804f1426a2bc876626cebca
Radib Bin Kabir (DSi) <radib.kabir@dsinnovators.com>
2025-12-04 16:05:26 +0600

    completed voting

--- djangotutorial/polls/views.py
+++ djangotutorial/polls/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, get_list_or_404, get_object_or_404  # type: ignore
-from django.http import HttpResponse # pyright: ignore[reportMissingModuleSource]
+from django.http import HttpResponse, HttpResponseRedirect # pyright: ignore[reportMissingModuleSource]
 from django.http import Http404 # type: ignore
-from .models import Question
+from django.db.models import F  # type: ignore
+from django.urls import reverse
+from .models import Question, Choice
 
 
 def index(request):
@@ -22,10 +24,31 @@ def detail(request, question_id):
     return render(request, "polls/details.html", context)
 
 def results(request, question_id):
-    return HttpResponse(f"You're looking at the result of question {question_id}.")
+    # return HttpResponse(f"You're looking at the result of question {question_id}.")
+    ques = get_object_or_404(Question, pk=question_id)
+    context = {
+        "question" : ques,
+    }
+    return render(request, "polls/results.html", context)
 
 def vote(request, question_id):
-    return HttpResponse(f"You're voting on question {question_id}.")
+    # return HttpResponse(f"You're voting on question {question_id}.")
+    ques = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = ques.choice_set.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+        return render(
+            request,
+            "polls/details.html",
+            {
+                "question": ques,
+                "error_message": "You didn't select a choice."
+            }
+        )
+    else:
+        selected_choice.votes = F("votes") + 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse("polls:results", args=(ques.id,)))
 
 def hello(request):
     return HttpResponse("<p>Hello from Django!</p>")
