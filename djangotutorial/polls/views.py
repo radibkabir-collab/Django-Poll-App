5c450d0a11a8609779f685b95a544b45b6cbf1fb
Radib Bin Kabir (DSi) <radib.kabir@dsinnovators.com>
2025-12-04 11:47:18 +0600

    changed the question template and added htmx

--- djangotutorial/polls/views.py
+++ djangotutorial/polls/views.py
@@ -5,7 +5,7 @@ from .models import Question
 
 
 def index(request):
-    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    latest_question_list = Question.objects.order_by("pub_date")[:5]
     template = loader.get_template("polls/index.html")
     context = {"latest_question_list": latest_question_list}
     return HttpResponse(template.render(context, request))
@@ -20,3 +20,6 @@ def results(request, question_id):
 
 def vote(request, question_id):
     return HttpResponse(f"You're voting on question {question_id}.")
+
+def hello(request):
+    return HttpResponse("<p>Hello from Django!</p>")
