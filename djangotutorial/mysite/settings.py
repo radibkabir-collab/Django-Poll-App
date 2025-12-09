d6255320bdafae938d72b90ba49c48d056d35d37
Radib Bin Kabir (DSi) <radib.kabir@dsinnovators.com>
2025-12-09 17:52:09 +0600

    connected postgresql

--- djangotutorial/mysite/settings.py
+++ djangotutorial/mysite/settings.py
@@ -73,10 +73,21 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'poll_db',
+        'USER': 'polls_user',
+        'PASSWORD': 'radibbk',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
